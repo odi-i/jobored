42532b6878162f29b7254ae6aeb19edf16b18842
grom0330 <69398712+grom0330@users.noreply.github.com>
2023-05-28 19:10:05 +0300

    fix: fix notFound adaptive mistakes

--- src/components/Vacancies/Vacancies.tsx
+++ src/components/Vacancies/Vacancies.tsx
@@ -61,7 +61,7 @@ export default function Vacancies(props: VacanciesProps) {
           })}
       {!data.objects.length && !isRerender && (
         <div className={styles.notFound}>
-          <img src={notFound} />
+          <img className={styles.img} src={notFound} />
           <div className={styles.text}>Упс, здесь еще ничего нет!</div>
         </div>
       )}
